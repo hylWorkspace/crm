9551094f1cb1c609a0654c75417727dcf6d1b994
18326957029 <1214002213@qq.com>
2021-08-14 22:09:11 +0800

    尝试

---
+++ src/main/java/com/study/entity/User.java
@@ -0,0 +1,13 @@
+package com.study.entity;
+
+public class User {
+    /**
+     * git
+     */
+    private String id;
+    private String name;
+    private Integer age;
+    private String email;
+
+
+}
